d5218ce146d07a7dc59d0f01cdc5ef3a3b11d6cf
Mehdi Bekhtaoui <mehdi.bekhtaoui@epita.fr>
2021-10-30 18:29:19 +0200

    [FIX] Maybe startup will be simpler now ?

--- gcloud.sh
+++ gcloud.sh
@@ -1,9 +1,11 @@
 # Script typed in Google Cloud startup script
 
-sudo add-apt-repository ppa:longsleep/golang-backports
-sudo apt update
-sudo apt install golang-go
+sudo add-apt-repository ppa:longsleep/golang-backports -y
+sudo apt update -y
+sudo apt install golang-go -y
+sudo apt install git -y
 
+cd /home/verycols
 git clone https://github.com/DemyCode/quizzfasttrack.git
 cd quizzfasttrack
 go build server/main.go
